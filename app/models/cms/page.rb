43f8aabe89de78df86c2cabfde4a3095bb07b44e
Adrian Madrid <aemadrid@gmail.com>
2012-09-11 17:35:23 -0600

    ...

--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -12,7 +12,8 @@ class Cms::Page < ActiveRecord::Base
                   :blocks_attributes,
                   :is_published,
                   :target_page_id,
-                  :category_ids
+                  :category_ids,
+                  :force_ssl
   
   cms_acts_as_tree :counter_cache => :children_count
   cms_is_categorized
